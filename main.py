6a1fa038a7cc7a3a48cf27f00d665e4aedd7dee8
brenno-ostemberg <brenno.ostemberg.dev@gmail.com>
2024-10-30 09:31:16 -0400

    feat: implementando função que gera gráficos do tempo de execução e valor da venda

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
 import random
 import time
 import numpy
+import matplotlib.pyplot as plt
+
+# Função para gerar gráficos
+def gerar_graficos(tamanhos, valores_dinamica, valores_guloso, tempos_dinamica, tempos_guloso):
+
+    # Gráfico 1: Valor total de venda
+    plt.figure(figsize=(10, 5))
+    plt.plot(tamanhos, valores_dinamica, label='Dynamic Programming', marker='o')
+    plt.plot(tamanhos, valores_guloso, label='Greedy', marker='x')
+    plt.xlabel('n')
+    plt.ylabel('Valor Total de Venda')
+    plt.title('Valor Total de Venda - Dynamic Programming vs. Greedy')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+    # Gráfico 2: Tempo de Execução
+    plt.figure(figsize=(10, 5))
+    plt.plot(tamanhos, tempos_dinamica, label='Dynamic Programming', marker='o')
+    plt.plot(tamanhos, tempos_guloso, label='Greedy', marker='x')
+    plt.xlabel('n')
+    plt.ylabel('Tempo de Execução)')
+    plt.title('Tempo de Execução - Dynamic Programming vs. Greedy')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
 
 # Função para gerar preços aleatórios
 def gerar_precos(n):
-    precos = [random.randint(1, 3 * n) for i  in range(1, n+1)]
+    precos = [random.randint(1, 3 * n) for i in range(1, n+1)]
     precos.sort()
     return precos
 
@@ -53,8 +79,15 @@ print(f"Resultado Guloso: {resultado_guloso}")
 
 # Execução dos experimentos
 def executar_experimentos(inc, fim, stp):
+
+    tamanhos = []
+    valores_dinamica = []
+    valores_guloso = []
+    tempos_dinamica = []
+    tempos_guloso = []
+
     print(" n     vDP  tDP      vGreedy tGreedy       %")
-    print("-" * 60)
+    print("-" * 44)
 
     for n in range(inc, fim + 1, stp):
 
@@ -72,10 +105,21 @@ def executar_experimentos(inc, fim, stp):
 
         print(f"{n:5d} {vDP:5d} {tDP:8.6f} {vGreedy:7d} {tGreedy:8.6f} {porcentagem:6.2f}")
 
+        tamanhos.append(n)
+        valores_dinamica.append(vDP)
+        valores_guloso.append(vGreedy)
+        tempos_dinamica.append(tDP)
+        tempos_guloso.append(tGreedy)
+
+    return tamanhos, valores_dinamica, valores_guloso, tempos_dinamica, tempos_guloso
+
 # Parâmetros dos experimentos
 inc = 1000
-fim = 15000
-spt = 1000
+fim = 20000
+stp = 1000
 
-# Execução dos experimentos
-executar_experimentos(inc, fim, spt)
+# Execução dos experimentos e coleta dos dados
+tamanhos, valores_dinamica, valores_guloso, tempos_dinamica, tempos_guloso = executar_experimentos(inc, fim, stp)
+
+# Geração dos gráficos
+gerar_graficos(tamanhos, valores_dinamica, valores_guloso, tempos_dinamica, tempos_guloso)
